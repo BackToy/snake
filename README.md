26f9ad8cbc9f70f8a3e012f446d8f4e4aec679e2
kearney <191615342@qq.com>
2020-12-30 10:49:19 +0800

    实现蛇身整体的移动

--- README.md
+++ README.md
@@ -3,16 +3,19 @@
 贪吃蛇，基于python3-pygame
 ## 程序结构
 ### 蛇
-最基本的可以用数组，看到有用队列的，当然也可以使用指针。
+最基本的可以用数组，看到有用队列的，当然也可以使用链表。（Python没有指针、数组类型）
 - 数组：0存蛇尾，新插入的蛇头放在数组后面。  
 移动时把数组前移；咬自己时蛇头位置和蛇身某个位置的数组相同。
-- 队列：新加入的头放在队列尾部
+- 队列：新加入的头放在队列尾部。python队列嵌套涉及到深浅复制！！！
 ## License
 GPL 3.0
 ## References
 - [贪吃蛇python代码分析](https://blog.csdn.net/weixin_41925383/article/details/99938886)
 - [ guliang21 /pygame ](https://github.com/guliang21/pygame)
+- [Python中list的复制及深拷贝与浅拷贝探究](https://www.cnblogs.com/Black-rainbow/p/9577029.html)
 
 ## ChangeLog
+- 2020/12/30  
+实现蛇身整体的移动
 - 2020/12/9  
 实现蛇头的移动控制
--- main.py
+++ main.py
@@ -12,26 +12,29 @@
 import pygame
 from pygame.locals import K_SPACE, K_UP, K_w, K_DOWN, K_s,\
     K_LEFT, K_a, K_RIGHT, K_d, K_ESCAPE
+import copy
+WIDTH = 960  # 窗体宽度
+HEIGHT = 640  # 窗体高度
+SIZE = 40  # 方格大小
+LINEWIDTH = 1  # 线宽
+FPS = 30  # 帧率
 
-WIDTH = 960
-HEIGHT = 640
-SIZE = 40
-LINEWIDTH = 1
-FPS = 30
+CBACK = (153, 255, 0)  # 背景色
+CLINE = (0, 0, 255)  # 线条颜色
+CSNAKE = (245, 245, 220)  # 蛇的颜色
+CTARGET = (255, 0, 0)  # 目标颜色
+POSITION = [[10, 3], [10, 4], [10, 5]]  # 蛇的身体坐标，列表中嵌套列表
+DIRECTION = 3  # 方向，0～3 上下左右
 
-CBACK = (153, 255, 0)
-CLINE = (0, 0, 255)
-CSNAKE = (245, 245, 220)
-CTARGET = (255, 0, 0)
-POSITION = [[10, 5]]
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-if WIDTH % 40 == 0 and HEIGHT % 40 == 0:
+if WIDTH % 40 == 0 and HEIGHT % 40 == 0:  # SIZE是否可以被整除
     pass
-else:
+else:  # 无法整除就结束
     print("Size not proper")
+    exit(0)
 
 while True:
     screen.fill(CBACK)  # 清空画面为背景色
@@ -42,39 +45,50 @@ while True:
     for y in range(SIZE, HEIGHT, SIZE):
         pygame.draw.line(screen, CLINE, (0, y), (WIDTH, y), LINEWIDTH)
 
-    # 绘制蛇头
-    pygame.draw.rect(screen, CSNAKE,
-                     (POSITION[0][0] * SIZE + 1, POSITION[0][1] * SIZE + 1,
-                      SIZE - 1, SIZE - 1), 0)
+    # 绘制蛇
+    tmp_len = len(POSITION)
+    POSHEAD = tmp_len - 1  # 蛇头位于数组的第POSHEAD个位置
+    for i in range(tmp_len):
+        pygame.draw.rect(screen, CSNAKE,
+                         (POSITION[i][0] * SIZE + 1, POSITION[i][1] * SIZE + 1,
+                          SIZE - 1, SIZE - 1), 0)
 
     # 获取键盘输入
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # 右上角x 退出程序
             pygame.quit()
             exit(0)
-        if event.type == pygame.KEYDOWN:  # 键盘事件
-            if pygame.key.get_pressed()[K_SPACE]:
+        if event.type == pygame.KEYDOWN:  # 键盘事件，获取方向
+            if event.key == K_SPACE:
                 print("space")
-            elif pygame.key.get_pressed()[K_ESCAPE]:  # 键盘左上角Esc 退出程序
+            elif event.key == K_ESCAPE:  # 键盘左上角Esc 退出程序
                 print("esq")
                 pygame.quit()
                 exit(0)
-            elif pygame.key.get_pressed()[K_UP] or pygame.key.get_pressed(
-            )[K_w]:
-                print("up")
-                POSITION[0][1] -= 1
-            elif pygame.key.get_pressed()[K_DOWN] or pygame.key.get_pressed(
-            )[K_s]:
-                print("down")
-                POSITION[0][1] += 1
-            elif pygame.key.get_pressed()[K_LEFT] or pygame.key.get_pressed(
-            )[K_a]:
-                print("left")
-                POSITION[0][0] -= 1
-            elif pygame.key.get_pressed()[K_RIGHT] or pygame.key.get_pressed(
-            )[K_d]:
-                print("Right")
-                POSITION[0][0] += 1
+            elif event.key in (K_UP, K_w):
+                DIRECTION = 0
+            elif event.key in (K_DOWN, K_s):
+                DIRECTION = 1
+            elif event.key in (K_LEFT, K_a):
+                DIRECTION = 2
+            elif event.key in (K_RIGHT, K_d):
+                DIRECTION = 3
+
+    # 修改蛇的位置
+    POSITION.reverse()  # 旋转、弹出蛇尾
+    POSITION.pop()
+    POSITION.reverse()
+    tmp = copy.deepcopy(POSITION)  # 深拷贝
+    if DIRECTION == 0:
+        tmp[POSHEAD - 1][1] -= 1  # 蛇头位置变化
+    elif DIRECTION == 1:
+        tmp[POSHEAD - 1][1] += 1  # 蛇头位置变化
+    elif DIRECTION == 2:
+        tmp[POSHEAD - 1][0] -= 1  # 蛇头位置变化
+    elif DIRECTION == 3:
+        tmp[POSHEAD - 1][0] += 1  # 蛇头位置变化
+
+    POSITION.append(tmp.pop())  # 添加蛇头
 
     clock.tick(FPS)  # 以每秒30帧的速率进行绘制
     pygame.display.update()  # 更新画面
