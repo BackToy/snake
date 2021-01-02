61e9be84d15aaa493593d94ebda166e980ed841c
kearney <191615342@qq.com>
2021-01-02 15:13:23 +0800

    方向编码由一位变为两位；fix添加蛇身逻辑

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ HEIGHT = 640  # 窗体高度
 SIZE = 80  # 方格大小
 NUMX = int(WIDTH / SIZE) - 1  # x、y轴方格子数-1
 NUMY = int(HEIGHT / SIZE) - 1
-TARGET = [random.randint(0, NUMX), random.randint(0, NUMY)]  # 目标坐标
+TARGET = (random.randint(0, NUMX), random.randint(0, NUMY))  # 目标坐标
 isEat = True  # 第一次生成目标也进行蛇身检测
 isFail = False
 isPause = False
@@ -37,8 +37,8 @@ CBACK = (153, 255, 0)  # 背景色
 CLINE = (0, 0, 255)  # 线条颜色
 CSNAKE = (245, 245, 220)  # 蛇的颜色
 CTARGET = (255, 0, 0)  # 目标颜色
-POSITION = [[1, 3], [1, 4], [1, 5]]  # 蛇的身体坐标，列表中嵌套列表
-DIRECTION = 3  # 方向，0～3 上下左右
+POSITION = [(1, 3), (1, 4), (1, 5)]  # 蛇的身体坐标，列表中嵌套列表
+DIRECTION = (1, 0)  # 方向，x方向变化量、y方向变化量
 isMove = False
 
 
@@ -56,7 +56,7 @@ def savedata(filepath, data):
 
 def showStartScreen():
     """
-    现实开场动画
+    展示开场动画
     """
     titleFont = pygame.font.Font(None, 100)
     titleSurf1 = titleFont.render('Kearney!', True, CSNAKE, CTARGET)
@@ -120,22 +120,14 @@ showStartScreen()
 
 while True:
     screen.fill(CBACK)  # 清空画面为背景色
-
-    if isEat:  # 被吃掉之后随机生成目标
-        while TARGET in POSITION:  # 如果目标和蛇重叠一直循环
-            TARGET[0] = random.randint(0, NUMX)
-            TARGET[1] = random.randint(0, NUMY)
-        isEat = False
-
     # 绘制网格
     for x in range(SIZE, WIDTH, SIZE):
         pygame.draw.line(screen, CLINE, (x, 0), (x, HEIGHT), LINEWIDTH)
     for y in range(SIZE, HEIGHT, SIZE):
         pygame.draw.line(screen, CLINE, (0, y), (WIDTH, y), LINEWIDTH)
-
     # 绘制蛇 和 目标
     tmp_len = len(POSITION)
-    POSHEAD = tmp_len - 2  # 现在蛇头位于数组的第（tmp_len-1）个位置
+    POSHEAD = tmp_len - 1  # 现在蛇头的位置
     for i in range(tmp_len):
         pygame.draw.rect(screen, CSNAKE,
                          (POSITION[i][0] * SIZE + 1, POSITION[i][1] * SIZE + 1,
@@ -143,7 +135,6 @@ while True:
     pygame.draw.rect(
         screen, CTARGET,
         (TARGET[0] * SIZE + 1, TARGET[1] * SIZE + 1, SIZE - 1, SIZE - 1), 0)
-
     # 咬蛇自尽判断
     try:
         tmp = copy.deepcopy(POSITION)
@@ -159,73 +150,52 @@ while True:
         if event.type == pygame.QUIT:  # 右上角x 退出程序
             terminate()
         if event.type == pygame.KEYDOWN:  # 键盘事件，获取方向
-            if event.key == K_SPACE:
+            if event.key == K_SPACE:  # 暂停/重新开始
                 isPause = not isPause  # 暂停
                 if isFail:  # 失败了重新开始
-                    POSITION = [[1, 3], [1, 4], [1, 5]]
-                    DIRECTION = 3
+                    POSITION = [(1, 3), (1, 4), (1, 5)]
+                    DIRECTION = (1, 0)
                     SCORE = 0
-                    while True:
-                        try:
-                            POSITION.index(TARGET)  # 在队列里返回下标，不在则抛出异常
-                            TARGET[0] = random.randint(1, 3)
-                            TARGET[1] = random.randint(1, 6)
-                        except (Exception):
-                            break
+                    while TARGET in POSITION:
+                        TARGET = (random.randint(0, NUMX),
+                                  random.randint(0, NUMY))
                     isFail = False
-
             elif event.key == K_ESCAPE:  # 键盘左上角Esc 退出程序
                 terminate()
-            elif event.key in (K_UP, K_w) and DIRECTION != 1 and isMove:
-                DIRECTION = 0
+            elif event.key in (K_UP, K_w) and DIRECTION != (0, 1) and isMove:
+                DIRECTION = (0, -1)
                 isMove = False
-            elif event.key in (K_DOWN, K_s) and DIRECTION != 0 and isMove:
-                DIRECTION = 1
+            elif event.key in (K_DOWN,
+                               K_s) and DIRECTION != (0, -1) and isMove:
+                DIRECTION = (0, 1)
                 isMove = False
-            elif event.key in (K_LEFT, K_a) and DIRECTION != 3 and isMove:
-                DIRECTION = 2
+            elif event.key in (K_LEFT, K_a) and DIRECTION != (1, 0) and isMove:
+                DIRECTION = (-1, 0)
                 isMove = False
-            elif event.key in (K_RIGHT, K_d) and DIRECTION != 2 and isMove:
-                DIRECTION = 3
+            elif event.key in (K_RIGHT,
+                               K_d) and DIRECTION != (-1, 0) and isMove:
+                DIRECTION = (1, 0)
                 isMove = False
     if not isPause:
         if TMPFRAME % SPEED == 0 and not isFail:  # 修改蛇的位置、蛇与目标碰撞检测
-            del POSITION[0]  # 删除旧蛇尾，现在蛇头位于数组的第（tmp_len-2）个位置
-            tmp = copy.deepcopy(POSITION)  # 深拷贝
-            if DIRECTION == 0:
-                tmp[POSHEAD][1] -= 1  # 蛇头位置变化
-            elif DIRECTION == 1:
-                tmp[POSHEAD][1] += 1  # 蛇头位置变化
-            elif DIRECTION == 2:
-                tmp[POSHEAD][0] -= 1  # 蛇头位置变化
-            elif DIRECTION == 3:
-                tmp[POSHEAD][0] += 1  # 蛇头位置变化
+            nextPos = (POSITION[POSHEAD][0] + DIRECTION[0],
+                       POSITION[POSHEAD][1] + DIRECTION[1])  # 蛇头的下一个位置
+            POSITION.append(nextPos)  # 添加移动后的新蛇头
+            # 边界检测
+            if nextPos[0] < 0 or nextPos[0] > NUMX or nextPos[
+                    1] < 0 or nextPos[1] > NUMY:
+                isFail = True
+                isPause = True
             # 目标碰撞检测
-            if tmp[POSHEAD][0] == TARGET[0] and tmp[POSHEAD][1] == TARGET[1]:
-                isEat = True
+            if nextPos == TARGET:
+                while TARGET in POSITION:  # 生成新目标如果目标和蛇重叠一直循环
+                    TARGET = (random.randint(0, NUMX), random.randint(0, NUMY))
                 SCORE += 1  # 分数加一
                 if SCORE > SCORE_MAX:
                     SCORE_MAX = SCORE
-            # 边框碰撞检测
-            if tmp[POSHEAD][0] < 0 or tmp[POSHEAD][0] > NUMX or tmp[POSHEAD][
-                    1] < 0 or tmp[POSHEAD][1] > NUMY:
-                isFail = True
-                isPause = True
-            POSITION.append(tmp.pop())  # 添加移动后的新坐标
+            else:  # 没吃到
+                del POSITION[0]  # 删除旧蛇尾
             isMove = True
-            if isEat:  # 吃掉目标后添加新蛇头坐标
-                tmp = copy.deepcopy(POSITION)
-                POSHEAD = len(POSITION) - 1
-                if DIRECTION == 0:
-                    tmp[POSHEAD][1] -= 1
-                elif DIRECTION == 1:
-                    tmp[POSHEAD][1] += 1
-                elif DIRECTION == 2:
-                    tmp[POSHEAD][0] -= 1
-                elif DIRECTION == 3:
-                    tmp[POSHEAD][0] += 1
-                POSITION.append(tmp.pop())
-
             TMPFRAME = 1  # 重置，防止溢出
         TMPFRAME += 1
 
